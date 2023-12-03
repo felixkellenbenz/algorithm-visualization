5602a6295b96832400976e74a6f41db2a0203ec6
Felix Kellenbenz <fe.kellenbenz.computer@outlook.de>
2023-12-03 23:14:15 +0100

    *Add basic methods and attributes Grid, GridBuilder, Node, ObstacleNode *Add color class

--- src/grid.hpp
+++ src/grid.hpp
@@ -1,41 +1,93 @@
+#include <SDL2/SDL_pixels.h>
 #include <SDL2/SDL_rect.h>
 #include <cstdint>
-#include <unordered_map>
 #include <SDL2/SDL.h>
-#include <tuple>
 #include <vector>
 
-
-class ScreenElement
+class Color 
 {
-public:
+private:
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+  uint8_t alpha;
 
-  virtual void render() = 0;
+public:
+  Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) :
+    red(r), green(g), blue(b), alpha(a) {}
+
+  Color(uint8_t r, uint8_t g, uint8_t b) :
+    red(r), green(g), blue(b), alpha(SDL_ALPHA_OPAQUE) {}
 
 };
 
+/*Abstraction for something that should be visible on the Screen*/
+class ScreenElement
+{
+public:
+  virtual void render(SDL_Renderer*) = 0;
+
+};
+
+
+/*Class that encapsulates a Rect*/
+class Node
+{
+private:
+  SDL_Rect rect;
+  std::vector<Node> neighbours;
+
+public:
+  void render();
+
+};
+
+/*A special class representing a Obstacle Node*/
+class ObstacleNode
+{
+private:
+
+public:
+  void render();
+
+};
 
 /* Class representing the Grid on
  * which algorithms will be executed */
 class Grid
 {
+private:
 
-private:	
-
+  std::vector<Node> free_rects;
+  std::vector<ObstacleNode> obstacle_rects;
+  ScreenElement* start_rect;
 
 public:
-
+  Grid();
+  ~Grid();
+  Grid(const Grid&);
+  Node& find_node(int x, int y);
+  void render();
 
 };
 
 /*Build a grid with only free rects*/
 class GridBuilder
 {
+private:
+  uint32_t grid_w;
+  uint32_t grid_h;
+  uint8_t border;
+  Color grid_color;
+  Grid grid;
 
+public:
+  void build_grid(); 
+  Grid export_grid();
 
 };
 
-/*Edit grid*/
+/*Class for modeling a Grid Editor*/
 class GridEditor
 {
 
@@ -43,26 +95,4 @@ class GridEditor
 
 };
 
-/*Render the grid with an SDL_Renderer*/
-class GridRenderer
-{
 
-
-
-};
-
-/*Class that encapsulates a Rect*/
-class Node
-{
-private:
-  // uderlying rect
-  SDL_Rect rect;
-
-public:
-};
-
-/*A special class representing a Obstacle Node*/
-class ObstacleNode
-{
-private:
-};
